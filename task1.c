a4bf1cf53d5a3a80ce90b0250fc1c9011ee15982
Sohakhan128 <sohakhan@eng.rizvi.edu.in>
2025-10-14 14:06:45 +0530

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main () {
+/*
+Name: Khan Soha Mohammed Amin
+Age: 18
+Class: F.E. (Computer2)
+Batch: D
+UIN: 251P104
+*/
+
+printf("Name: Khan Soha Mohammed Amin\n");
+printf("Age: 18\n");
+printf("Class: F.E. (Computer2)\n");
+printf("Batch: D\n");
+printf("UIN: 251P104\n");
+
+return 0;
+
+}
