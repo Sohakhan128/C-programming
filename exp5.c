8eda8d87a546a03553c54dade86f5d0d37b86631
Sohakhan128 <sohakhan@eng.rizvi.edu.in>
2025-11-04 19:33:50 +0530

    Add files via upload

---
+++ exp5.c
@@ -0,0 +1,32 @@
+/*Name: Khan Soha Mohammed Amin
+      Class: F.E. (Computer2)
+      Batch: D
+      Roll no.: 27
+      UIN: 251P104
+    */
+#include<stdio.h>
+
+int iterative_fact(int num) {
+    int fact = 1;
+    for(int i = num; i>= 1; i--) {
+        fact = fact * i;
+    }
+    return fact;
+}
+
+int main() {
+    int a = 5;
+    printf(" Factorial of %d is %d", a, iterative_fact(a));
+
+    return 0;
+}
+
+int fact(int num) {
+    if(num == 0 || num == 1) {
+        return 1;
+    } else {
+        return num * fact(num - 1);
+    }
+     return 0;
+}
+
