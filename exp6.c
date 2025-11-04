e1c03367193a8d7a6ddf4f3bd312c97f4961bb58
Sohakhan128 <sohakhan@eng.rizvi.edu.in>
2025-11-04 19:48:19 +0530

    Add files via upload

---
+++ exp6.c
@@ -0,0 +1,25 @@
+/*Name: Khan Soha Mohammed Amin
+      Class: F.E. (Computer2)
+      Batch: D
+      Roll no.: 27
+      UIN: 251P104
+    */
+
+#include<stdio.h>
+
+void counter() {
+    static int count = 0;
+    count ++;
+    printf("Count = %d\n", count);
+}
+
+int main() {
+    counter();
+    counter();
+    counter();
+
+    return 0;
+}
+
+
+
