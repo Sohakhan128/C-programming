3d1cc643725e0568768cbe8e5d0a367f4b27d15d
Sohakhan128 <sohakhan@eng.rizvi.edu.in>
2025-11-19 19:44:36 +0530

    Add files via upload

---
+++ exp8a.c
@@ -0,0 +1,27 @@
+/*Name: Khan Soha Mohammed Amin
+      Class: F.E. (Computer2)
+      Batch: D
+      Roll no.: 27
+      UIN: 251P104
+    */
+    
+
+#include <stdio.h>
+
+int main() {
+   char str[100];
+   int i = 0;
+
+   printf("Enter a string: ");
+   scanf("%s", str);
+
+   while (str[i] != '\0') {
+       i++;
+   }
+   printf("Lenth = %d", i);
+    return 0;
+}
+
+
+
+
