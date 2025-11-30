d1b8003c2769a823bd8aa80a3117e9c684cf3402
Sohakhan128 <sohakhan@eng.rizvi.edu.in>
2025-11-30 18:05:01 +0530

    Add files via upload

---
+++ exp10a.c
@@ -0,0 +1,27 @@
+/*Name: Khan Soha Mohammed Amin
+      Class: F.E. (Computer2)
+      Batch: D
+      Roll no.: 27
+      UIN: 251P104
+    */
+    
+#include<stdio.h>
+
+int addNumbers(int *a, int*b) {
+    int sum = *a + *b;
+    return sum;
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Enter the numbers: ");
+    scaf("%d %d", &num1, &num2);
+
+    printf("Result is: %d", addNumbers(&num1, &num2));
+
+    return 0;
+}
+
+
+
